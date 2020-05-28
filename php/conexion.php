c1594f6ee8fbe4023f4d1bce65b6a1268acd7c2f
franciscoltd-lab <54999653+franciscoltd-lab@users.noreply.github.com>
2020-05-28 00:41:45 -0500

    Cambio de contraseña del localhost

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion = new mysqli("localhost","ssbjaliscocom","Guadalajara1","ssbjalis_encuestas");
+    $conexion = new mysqli("localhost","ssbjaliscocom","GXKk02TEcu93","ssbjalis_encuestas");
     if($conexion->connect_errno):
         echo "Error al conectarse a la base de datos debido al error: ". $conexion->connect_error;
     endif;  
