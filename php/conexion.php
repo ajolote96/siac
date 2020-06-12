69c15c94eaf807918bcecc5cd67028023bde586f
Francisco Reyna <francisco.reyna@alumnos.udg.mx>
2020-06-12 09:02:54 -0500

    Cambio de contraseña de BDA

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion = new mysqli("localhost","ssbjaliscocom","GXKk02TEcu93","ssbjalis_encuestas");
+    $conexion = new mysqli("localhost","ssbjaliscocom","Z5jtrL!@W@*22DDxW6KgsEzco","ssbjalis_encuestas");
     if($conexion->connect_errno):
         echo "Error al conectarse a la base de datos debido al error: ". $conexion->connect_error;
     endif;  
