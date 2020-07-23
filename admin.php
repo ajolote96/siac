96f34394637de27e5b3696b56927b86abbd84e10
franciscoltd-lab <54999653+franciscoltd-lab@users.noreply.github.com>
2020-07-23 08:47:30 -0500

    Cambiando el Menú Admin

--- admin.php
+++ admin.php
@@ -14,115 +14,176 @@
         
         
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-    <!-- Gráficas -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/highcharts-3d.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>   
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Admin CAC</title>
+    <title>Administración</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/styles-admin.css">
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 </head>
 
 <body>
-
-    <header class="container">
-        <div>
-            <a href="admin.php">
-                <img src="img/cfe-contigo.png" alt="Logo CFE contigo" class="float-left logo-cfe-contigo">
-            </a>
-            <img src="img/suterm-logo.png" alt="Logo Suterm" class="float-right suterm-logo">
-        </div>
-    </header>
-
-
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-            <button class="navbar-toggler" data-target="#menu" data-toggle="collapse" type="button">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="menu">
-                <ul class="navbar-nav mr-auto">                   
-
-                     <li class="nav-item dropdown">
-                        <a href="" class="nav-link dropdown-toggle" data-target="desplegable" data-toggle="dropdown">Evaluación por Cac</a>
-                        <div class="dropdown-menu">
-                             <a class="dropdown-item" href="#" id="tabla">Tabla</a>  
-                             <a class="dropdown-item" href="#" id="enlace-grafico">Gráfica</a> 
-                        </div>
-                    </li> 
-
-                    <li class="navbar-item">
-                      <a href="#" id="datos-ejecutivo" class="nav-link">Evaluación de Ejecutivos</a> 
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a href="" class="nav-link dropdown-toggle" data-target="desplegable" data-toggle="dropdown">Sesiones Activas por CAC</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#" nombre="camichines" id="">Camichines</a>    
-                            <a class="dropdown-item" href="#" nombre="paradero" id="">Paradero</a>
-                            <a class="dropdown-item" href="#" nombre="revolucion" id="">Revolución</a>
-                            <a class="dropdown-item" href="#" nombre="tlaquepaque" id="">Tlaquepaque</a>
-                            <a class="dropdown-item" href="#" nombre="toluquilla" id="">Toluquilla</a>
-                            <a class="dropdown-item" href="#" nombre="tonala" id="">Tonalá</a>
-                            <a class="dropdown-item" href="#" nombre="santa fe" id="">Santa Fe</a>
-                        </div>
-                    </li>
-<!-- 
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle " data-target="desplegable" data-toggle="dropdown">Administración</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Empleados</a>
-                            <a class="dropdown-item" href="#">Usuarios</a>
-                        </div>
-                    </li> -->
-
-                    <li class="navbar-item ">
-                        <a href="static/php/cerrar-sesion.php" class="nav-link">Cerrar Sesión</a>
-                    </li>
-
-
-                </ul>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h2>Menú</h2>
             </div>
+
+            <ul class="list-unstyled components">
+                <p>Panel Administrativo</p>
+                
+                <li class="active">
+                    <a href="#homeSubmenuMatrix" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Motivos de Visitas</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenuMatrix">
+                        
+                        <!-- <li>
+                            <a href="#">Motivo de Visitas</a>
+                            Mostrar resultado de visitas, a qué vino cada cliente, registrar, por día y seleccionar rango
+                        </li> 
+                        -->
+                         <li>
+                            <a href="#">División</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Zona</a>
+                        </li>
+
+                        <li>
+                            <a href="#">CAC</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Generar Reporte Específico</a>
+                        </li>
+                       
+                    </ul>
+                </li>                                                                                    
+                    
+                <li class="active">
+                    <a href="#homeSubmenuEjecutivo" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Evaluación de Ejecutivo</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenuEjecutivo">
+                        
+                        <!-- <li>
+                            <a href="#">Motivo de Visitas</a>
+                            Mostrar resultado de visitas, a qué vino cada cliente, registrar, por día y seleccionar rango
+                        </li> 
+                        -->
+                         <li>
+                            <a href="#">División</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Zona</a>
+                        </li>
+
+                        <li>
+                            <a href="#">CAC</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Generar Reporte Específico</a>
+                        </li>
+                       
+                    </ul>
+                </li>  
+                
+                <!-- <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Gestión de Ejecutivo</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        
+                        <li>
+                            <a href="#">Alta</a>
+                        </li>
+                        <li>
+                            <a href="#">Baja</a>
+                        </li>
+                        <li>
+                            <a href="#">Modificación</a>
+                        </li>
+                    </ul>
+                </li> -->
+                <li id="ejecutivo">
+                    <a href="#">Gestión de Ejecutivo</a>
+                </li>
+
+                <li>  
+                    <a href="#">Gestión de Encuestas</a>
+                    <!-- Alta, baja, modificar preguntas -->
+                </li> 
+
+                <li>
+                    <a href="#">Gestión de Sitios</a>
+                </li>
+
+                <li class="active">
+                    <!-- Sólo  jefes de cac, sólo podrá modificar lo de sus cacs  -->
+                    <a href="#homeSubmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Configuración</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu2">
+                        
+                        <li>
+                            <a href="#">Usuarios</a>
+                        </li>
+                       
+                    </ul>
+                </li>               
+                
+            </ul>
+
+          
         </nav>
+        
+        <!-- Page Content  -->
+        <div id="content">
+            <div class="header-content">
+                <h2>Inicio</h2>
+            </div>
+
+            <div class="logo">
+                <a href="admin.php">
+                    <img style="width: 20%; margin-left: 50px;" src="img/cfe-distribucion.png" alt="LOGO">
+                </a>
+            </div>
+            <!-- <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+                <div class="container-fluid">       
+                    <img style="width: 20%;" src="cfe-distribucion.png" alt="LOGO">                              
+                </div>
+            </nav> -->
+            
+        </div>
+
     </div>
 
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-    <div style="display:none" id="contenido"></div>
-    <div style="display:none" id="datos">Datos</div>
-
-    </body>
-    
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- <script src="js/main.js"></script> -->
-
-    <script>
-        $(document).on('click', '#tabla', function(){
-            $("#datos").hide("slow");
-            $("#contenido").show();
-            $("#contenido").load("html/datos-ejecutivo.html");
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
         });
 
-        $(document).on('click', '#enlace-grafico', function(){
-            $("#contenido").hide("slow");
-            $("#datos").load("html/datos-ejecutivo.html");
-            $("#datos").show();
-        }); 
+        $('#ejecutivo').click(function () { 
 
+        });
     </script>
-   
+</body>
 
-</html
+</html>
