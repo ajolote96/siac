fa7517c476ede8e01f3eba131147bba9401a3f26
franciscoltd-lab <54999653+franciscoltd-lab@users.noreply.github.com>
2020-08-16 17:15:14 -0500

    Guardar datos en BDA, quejas e información y generar turno consecutivo

--- php/guardar-motivo.php
+++ php/guardar-motivo.php
@@ -5,8 +5,8 @@ session_start();
 
 
 
-$motivo_principal = $_POST['motivo'];
-$motivo_general = $_POST['motivo2'];
+$motivo = $_POST['motivo'];
+$detalle = $_POST['detalle'];
 $cac = $_SESSION['cac'];
 
 date_default_timezone_set("America/Mexico_City");
@@ -16,10 +16,10 @@ $date = date("Y-m-d", $time);
 
 if(isset($_POST['tipo'])){
     $tipo=$_POST['tipo'];
-    $query = "INSERT INTO motivos VALUES('".$motivo_principal."','".$motivo_general."','".$date."', '".$cac."', '".$tipo."')";
+    $query = "INSERT INTO motivos VALUES('".$motivo."','".$detalle."','".$date."', '".$cac."', '".$tipo."')";
 }
 else{
-    $query = "INSERT INTO motivos(motivo, detalle, fecha, cac) VALUES('".$motivo_principal."','".$motivo_general."','".$date."', '".$cac."')";
+    $query = "INSERT INTO motivos(motivo, detalle, fecha, cac) VALUES('".$motivo."','".$detalle."','".$date."', '".$cac."')";
 }
 
 $conexion->query($query);
