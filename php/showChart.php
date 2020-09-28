268ed0f0d2be5471174416d2c134175298db5054
franciscoltd-lab <>
2020-09-28 18:53:18 -0500

    Agregando los filtros a las gráficas, corrigiendo errores

--- php/showChart.php
+++ php/showChart.php
@@ -11,15 +11,19 @@ $cac = $_POST['cac'];
 $inicio = $_POST['fecha-inicio'];
 $fin = $_POST['fecha-fin'];
 
-if(isset($_POST['division'])){
-  $query = "SELECT motivo, COUNT(motivo) as motivoContador FROM motivos WHERE cac IN (SELECT cac FROM cac  WHERE division = '".$division."') AND fecha>='".$inicio."' and fecha<='".$fin."' GROUP BY motivo";
+if(isset($_POST['cac'])){
+  $query = "SELECT motivo, COUNT(motivo) as motivoContador FROM motivos WHERE cac='".$cac."' and fecha>='".$inicio."' and fecha<='".$fin."' GROUP BY motivo";
 }
 else if(isset($_POST['zona'])){
   $query = "SELECT motivo, COUNT(motivo) as motivoContador FROM motivos WHERE cac IN (SELECT cac FROM cac  WHERE zona = '".$zona."') AND fecha>='".$inicio."' and fecha<='".$fin."' GROUP BY motivo";
 }
 else{
-  $query = "SELECT motivo, COUNT(motivo) as motivoContador FROM motivos WHERE cac='".$cac."' and fecha>='".$inicio."' and fecha<='".$fin."' GROUP BY motivo";
+  $query = "SELECT motivo, COUNT(motivo) as motivoContador FROM motivos WHERE cac IN (SELECT cac FROM cac  WHERE division = '".$division."') AND fecha>='".$inicio."' and fecha<='".$fin."' GROUP BY motivo";
 }
+
+
+
+
 
 $resultado = $conexion->query($query);
 
