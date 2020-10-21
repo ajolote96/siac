19fedebd4eba8bb1f7d383094a659b12b957b1db
Heriberto Hurtado <47532782+betohurtado3@users.noreply.github.com>
2020-10-21 16:37:47 -0500

    Update fetch_cac.php

--- fetch_cac.php
+++ fetch_cac.php
@@ -1,5 +1,8 @@
 <?php
-    $connect = mysqli_connect("localhost", "root", "", "ssbjalis_encuestas");
+    $conexion = new mysqli("localhost","ssbjaliscocom","Z5jtrL!@W@*22DDxW6KgsEzco","ssbjalis_encuestas");
+    if($conexion->connect_errno):        
+        echo "Error al conectarse a la base de datos debido al error: ". $conexion->connect_error;
+    endif; 
     $output = '';
     $sql = "SELECT * FROM tbl_cac WHERE zone_id = '".$_POST["zonaId"]."' ORDER BY cac_id";
     $result = mysqli_query($connect, $sql);
